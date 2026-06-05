a905ce2a5d687985b0fdf4e9dad519913a62fcfe
cjh <ppk0584@gmail.com>
2026-06-06 08:00:28 +0900

    Simplify home hero for trust and choice

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.index = (req, res) => {
-    res.render('index', { 
-        title: '프레시미트 - 맛과 신선함의 정석',
-        message: '최고급 품질의 정육을 만나보세요.'
+    res.render('index', {
+        title: '프레시미트 - 메뉴 고민을 덜어주는 정육점',
+        message: '오늘 메뉴부터 캠핑장 직송까지 한 번에 준비합니다.'
     });
 };
